c733ececb62ea9400b33b864c6e1182517d47459
xeceryn <adityamusthofa67@gmail.com>
2020-07-01 18:49:14 +0700

    add remote

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg')
 const server = new Client({
   user: 'rkfeitexlcbxxg',
-  host: 'ec2-184-73-192-172.compute-1.amazonaws.com',
+  host: 'localhost',
   database: 'd976u6oh61ei83',
   password: 'eb5c0bf57d9def02ae5545a40d732ffb10ce17a2f2d280034a9f807fd424e1f8',
   port: 5432
